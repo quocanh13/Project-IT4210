ece7988ddc0daf1d13fcbeee7c6cc66b150bf9f7
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-14 23:40:41 +0700

    chore: add comment

--- Core/Inc/lcd.h
+++ Core/Inc/lcd.h
@@ -36,10 +36,10 @@ extern I2C_HandleTypeDef hi2c1;
 #define LCD_SET_ROW_2               0xC0  // Ép con trỏ về vị trí đầu dòng 2
 // Mã lệnh
 
-void lcd_send_data(char data);
-void lcd_send_cmd(char cmd);
-void lcd_init(void);
-void lcd_set_cursor(int row, int col);
-void lcd_print(char *str);
+void lcd_send_data(char data);              // Hàm gửi dữ liệu xuống LCD
+void lcd_send_cmd(char cmd);                // Hàm gửi lệnh xuống LCD
+void lcd_init(void);                        // Hàm khởi tạo LCD ở chế độ 4 bit
+void lcd_set_cursor(int row, int col);      // Hàm thiết lập vị trí con trỏ
+void lcd_print(char *str);                  // Hàm in xâu ra màn hình tính từ vị trí con trỏ hiện tại
 
 #endif
