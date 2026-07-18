d9d9ed16c1ac37ba610f61b6412eaf01fb869872
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-18 21:07:17 +0700

    chore: add comment

--- Core/Inc/game.h
+++ Core/Inc/game.h
@@ -10,8 +10,8 @@ extern uint8_t level;
 unsigned int get_score();               // Hàm lấy điểm số hiện tại
 void reset_state();                     // Hàm reset trạng thái điểm và led
 void press_button(uint8_t button);      // Hàm xử lý sự kiện nhấn button
-void press_level_up_button();
-void press_level_down_button();
-void press_start_button();
+void press_level_up_button();           // Hàm xử lý sự kiện nhấn button tăng level
+void press_level_down_button();         // Hàm xử lý sự kiện nhấn button giảm level
+void press_start_button();              // Hàm xử lý sự kiện nhấn nút bắt đầu
 
 #endif  
